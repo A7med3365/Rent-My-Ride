a183724c68cafaff34c1bbe65b370d08ab050161
Ahmed Home PC <a7med3365@gmail.com>
2023-06-14 23:20:53 +0300

    solved the calander bug

--- controllers/bookingRequest.js
+++ controllers/bookingRequest.js
@@ -1,21 +1,22 @@
 //unavalible dates array contains objects with start and end dates, by defult it will contain
 //the current date as a ending date and start from 2023-01-01
-const unavailableDatesRanges = [
-  {
-    from: "2023-01-01",
-    to: new Date().toISOString().slice(0, 10),
-  },
-];
 
 const Bookings = require("../models/Booking");
 
 exports.request_booking_get = async (req, res) => {
+  const unavailableDatesRanges = [
+    {
+      from: "2023-01-01",
+      to: new Date().toISOString().slice(0, 10),
+    },
+  ];
+
   const carId = req.query.carId;
   const ownerId = req.query.ownerId;
 
   const oldBookings = await Bookings.find({ car: carId, owner: ownerId });
-
-  console.log(oldBookings);
+  // console.log("----------------------------------------");
+  // console.log(oldBookings);
 
   //go through each booking and add the dates to the unavailableDates array
   oldBookings.forEach((booking) => {
@@ -23,6 +24,8 @@ exports.request_booking_get = async (req, res) => {
     const endDate = booking.endDate;
     unavailableDatesRanges.push({ from: startDate, to: endDate });
   });
+
+  // console.log(unavailableDatesRanges);
 
   res.render("cars/bookingRequest", {
     unavailableDatesRanges: JSON.stringify(unavailableDatesRanges),
