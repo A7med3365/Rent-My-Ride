35fcaaa39db09f07c6e06d019ba16efc83a6f259
Ahmed Home PC <a7med3365@gmail.com>
2023-06-14 21:46:37 +0300

    f

--- controllers/adminCarList.js
+++ controllers/adminCarList.js
@@ -3,5 +3,5 @@ const Car = require("../models/Car");
 
 exports.adminCarList = async function (req, res) {
   const cars = await Car.find();
-  res.render("admin/carsIndex");
+  res.render("admin/carsIndex", { cars });
 };
--- controllers/adminFilesList.js
+++ controllers/adminFilesList.js
@@ -1,2 +1,7 @@
 // adminFilesList.js
-exports.adminFilesList = function (req, res) {}
+const File = require("../models/File");
+
+exports.adminFilesList = async function (req, res) {
+  const files = await File.find();
+  res.render("admin/filesIndex", { files });
+};
