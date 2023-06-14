3d1821939c89d718e273f1b5e0b27e084fa089c5
Ahmed Home PC <a7med3365@gmail.com>
2023-06-14 23:04:09 +0300

    fixed the booking bug caused by merging this morning

--- controllers/bookingRequestPost.js
+++ controllers/bookingRequestPost.js
@@ -2,22 +2,28 @@
 const Booking = require("../models/Booking");
 
 exports.bookingRequestPost = async function (req, res) {
-  const dateRange = req.body["date-range"].split(" to ");
-  const booking = await new Booking({
-    car: req.body.car,
-    owner: req.body.owner,
-    renter: req.user._id,
-    startDate: dateRange[0],
-    endDate: dateRange[1],
-  });
-  await booking
-    .save()
-    .then(function (newBooking) {
-      console.log("a new booking has been saved: ", newBooking);
-    })
-    .catch(function (err) {
-      console.log(`an error occurred: ${err.message}`);
+  try {
+    console.log(req.body);
+    const dateRange = req.body["date-range"].split(" to ");
+    const booking = await new Booking({
+      car: req.body.car,
+      owner: req.body.owner,
+      renter: req.user._id,
+      startDate: dateRange[0],
+      endDate: dateRange[1],
     });
+    await booking
+      .save()
+      .then(function (newBooking) {
+        console.log("a new booking has been saved: ", newBooking);
+      })
+      .catch(function (err) {
+        console.log(`an error occurred: ${err.message}`);
+      });
 
-  res.redirect("/user/history");
+    res.redirect("/user/history");
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
 };
--- controllers/historyGet.js
+++ controllers/historyGet.js
@@ -6,6 +6,5 @@ exports.historyGet = async function (req, res) {
     .populate("car")
     .populate("owner");
 
-  console.log(userBookings);
   res.render("user/history", { userBookings });
 };
